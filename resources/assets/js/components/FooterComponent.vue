38d694abcc47321ab73f48603932c68a6ddf1db0
Robert Kraig <robertkraig@gmail.com>
2018-07-06 16:15:56 -0700

    updating footer, adding font-awesome

--- resources/assets/js/components/FooterComponent.vue
+++ resources/assets/js/components/FooterComponent.vue
@@ -1,13 +1,129 @@
 <style lang="scss" scoped>
     .footer {
+        position: relative;
+        color:var(--white);
         height: 486px;
         background-image: linear-gradient(to top, #03bf3c, #009de1);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .share-icons {
+            padding-top: 225px;
+            width: 600px;
+            text-align: center;
+            position: absolute;
+            h4 {
+                font-size: 22px;
+                font-weight: 300;
+                padding:0;
+                margin: 0 0 20px;
+            }
+            .icon {
+                font-size: 40px;
+                color:var(--white);
+                margin: 0 25px;
+            }
+            .copyright {
+                margin-top: 50px;
+                font-size: 16px;
+                font-weight: 500;
+            }
+        }
+        .link-list-container {
+            display: grid;
+            grid-template-columns: repeat(2, auto);
+            grid-template-rows: auto;
+            position: absolute;
+            width: 550px;
+            top: 50px;
+            left: 160px;
+            z-index: 100;
+            .link-list {
+                a {
+                    color:var(--white);
+                    text-decoration: none;
+                    font-size: 20px;
+                    font-weight: 300;
+                }
+
+                h4 {
+                    font-size: 20px;
+                    font-weight: 500;
+                    margin:0;
+                    padding:0;
+                    position: relative;
+
+                    .line {
+                        height: 2px;
+                        background-color: var(--white);
+                        position: absolute;
+                        bottom: -7px;
+                        left: 12px;
+                        display: block;
+                        width: 188px;
+                    }
+                }
+                &.site-map {
+
+                }
+                &.contact-us {
+
+                }
+
+                ul {
+                    list-style: none;
+                    padding: 0;
+                    margin: 32px 0 0 20px;
+                    li {
+                        list-style: none;
+                        padding:0;
+                        margin: 0 0 25px;
+                    }
+                }
+            }
+        }
     }
 </style>
 
 <template>
     <footer class="footer">
-
+        <div class="link-list-container">
+            <div class="link-list site-map">
+                <h4>SITE MAP <span class="line"></span></h4>
+                <ul>
+                    <li>
+                        <router-link to="/">Our Story</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/the-team">The Team</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/join-us">Join Us</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/donate">Donations</router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="link-list contact-us">
+                <h4>CONTACT US<span class="line"></span></h4>
+                <ul>
+                    <li><a href="#">Email</a></li>
+                    <li><a href="#">Phone</a></li>
+                    <li><a href="#">Volunteer</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="share-icons">
+            <h4>JOIN THE CONVERSATION</h4>
+            <i class="icon fab fa-twitter"></i>
+            <i class="icon fab fa-instagram"></i>
+            <i class="icon fab fa-facebook-square"></i>
+            <div class="copyright">
+                &copy; 2018 Legislation for Equality Advance Progress. All Rights Reserved.
+            </div>
+        </div>
     </footer>
 </template>
 
