bb2cc990392e95b99afad9c115fb7798966ab4d1
Robert Kraig <robertkraig@gmail.com>
2018-07-26 13:16:13 -0700

    Updating the layout styling

--- resources/assets/js/LayoutCalculator.vue
+++ resources/assets/js/LayoutCalculator.vue
@@ -9,6 +9,16 @@
     }
     .computed-mobile {
         padding-top: 72px;
+        .container {
+            max-width: 1440px;
+            margin: auto;
+            position: fixed;
+            top: 72px;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            overflow-y: auto;
+        }
     }
 </style>
 
@@ -16,8 +26,10 @@
     <div class="layout-calculator">
         <mq-layout class="computed-mobile" :mq="['m_tiny','m_small','m_bigger', 'tablet']">
             <menu-mobile-component />
-            <router-view name="mobile" />
-            <footer-mobile-component />
+            <section class="container">
+                <router-view name="mobile" />
+                <footer-mobile-component />
+            </section>
         </mq-layout>
         <mq-layout class="computed-standard" mq="standard">
             <menu-component />
