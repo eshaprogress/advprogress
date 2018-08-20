efd43c2712f366c368c1c7098d8bf709f06b9bb8
Robert Kraig <robertkraig@gmail.com>
2018-08-20 16:36:01 -0700

    Updating Application Name.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Advanced Progress</title>
+        <title>{{config('app.name')}}</title>
         <link rel="stylesheet" href="{{mix('/css/app.css')}}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
