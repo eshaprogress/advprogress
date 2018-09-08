f70e4b542da44e525d1f8af19f2463673eaaf9d3
Robert Kraig <robertkraig@gmail.com>
2018-09-08 14:01:40 -0700

    Fixing broken migration.

--- database/migrations/2018_09_08_180549_make_all_project_text_fields_nullable.php
+++ database/migrations/2018_09_08_180549_make_all_project_text_fields_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MakeAllTextProjectFieldsNullable extends Migration
+class MakeAllProjectTextFieldsNullable extends Migration
 {
     /**
      * Run the migrations.
