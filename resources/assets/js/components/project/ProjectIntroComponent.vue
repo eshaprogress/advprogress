e70f97e62bafbc92453d75f71f2d135f6c22319a
Advanceprogress <eshap.k@protonmail.com>
2018-10-31 14:28:56 -0400

    Fixing the intro page

--- resources/assets/js/components/project/ProjectIntroComponent.vue
+++ resources/assets/js/components/project/ProjectIntroComponent.vue
@@ -1,4 +1,5 @@
 <style lang="scss" scoped>
+@import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css.map";
     article {
         padding:0;
         max-width: 1150px;
@@ -14,6 +15,112 @@
             color: var(--blue);
         }
     }
+   
+    .title2 {
+  position: relative;
+  top: 0px;
+  right: 80px;
+  font-weight: 800;
+  font-size: 20px;
+  font-family: georgia;
+  text-align: right;
+  right-margin: 20px;
+  
+}
+@media only screen and (max-width: 550px) {
+  .title2 {
+    position: relative;
+    padding: 20px;
+    text-align: center;
+    right: 0px;
+    font-size: 18px;
+  } }
+.fancy-title {
+    position: relative;
+  text-align: center;
+  padding: 40px;
+  font-weight: 800;
+  font-size: 30px;
+  spacing: 10px;
+  font-family: georgia;
+  
+}
+.virginia{
+  position: relative;
+  top: 40px;
+  display: block;
+    margin-left: auto;
+    margin-right: auto;
+  width: 220px;
+}
+.virginia:hover { 
+  opacity: .4; }
+.prisoners{
+  padding: 20px;
+  position: relative;
+  top: 10px;
+  display: block;
+    margin-left: auto;
+    margin-right: auto;
+  width: 800px; 
+}
+.prisoners:hover { 
+  opacity: .4; }
+
+.article-text{
+  margin-top: 0px;
+  line-height: 2;
+  padding: 80px;
+   text-align: justify;
+  font-family: helvetica;
+  font-weight: 400;
+}
+.tagline1{
+  position: relative;
+top: -30px;
+  margin: 20px;
+   text-align: center;
+  font-family: helvetica;
+  font-weight: 400;
+}
+
+.article2{
+  margin-top: 0px;
+  margin: 40px;
+   text-align: center;
+  font-family: helvetica;
+  font-weight: 400;
+}
+.penn{
+  position: relative;
+  top: 30px;
+   display: block;
+    margin-left: auto;
+    margin-right: auto;
+  width:200px;
+}
+.penn:hover { 
+  opacity: .4; }
+.florida{
+  position: relative;
+  top: 20px;
+   display: block;
+    margin-left: auto;
+    margin-right: auto;
+  width: 200px;
+}
+.florida:hover { 
+  opacity: .4; }
+.cali{
+  position: relative;
+  top: 30px;
+   display: block;
+    margin-left: auto;
+    margin-right: auto;
+  width: 200px;
+}
+.cali:hover { 
+  opacity: .4; }
 </style>
 
 <template>
